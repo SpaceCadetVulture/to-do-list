86603a952924ab0e416de03341082991dc5f909b
SpaceCadetVulture <101519696+SpaceCadetVulture@users.noreply.github.com>
2022-10-25 21:23:59 +0200

    script.js update
    updated some scrit

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,33 @@
+function newItem() {
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
 
+    if (inputValue === '') {
+    alert("You must write something!");
+    } else {
+    $('#list').append(li);
+    }
 
-function newItem(){
+    /*function crossOut(){
+    li.addClass("strike");
+    }
+    li.on("dblclick", crossOut);*/
 
-//javascript
-//1. Adding a new item to the list of items: 
-   let li = document.createElement("li");
-   let inputValue = document.getElementById("input").value;
-   let text = document.createTextNode(inputValue);
-   li.appendChild(text);
+    // dblclick = double click vs click just ones
+    li.on("dblclick", function() {
+        li.addClass("strike");
+      });
 
-   if (inputValue === '') {
-     alert("You must write something!");
-   } else {
-     let list = document.querySelector('#list');
-     list.appendChild(li);
-   }
+    $('#list').sortable();
 
- //2. Crossing out an item from the list of items:
-   function crossOut() {
- 		li.classList.toggle("strike");
- 	}
+    let crossOutButton = $ ('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode("X"));
+    li.append(crossOutButton);
 
- 	li.addEventListener("dblclick",crossOut);
+    crossOutButton.on('click', deleteListItem);
 
- //3(i). Adding the delete button "X": 
-   let crossOutButton = document.createElement("crossOutButton");
- 	crossOutButton.appendChild(document.createTextNode("X"));
- 	li.appendChild(crossOutButton);
-
- 	crossOutButton.addEventListener("click", deleteListItem);
- //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-   function deleteListItem(){
- 		li.classList.add("delete")
- 	}
- // 4. Reordering the items: 
-   $('#list').sortable();
-
+    function deleteListItem(){
+        li.addClass('delete');
+    }
 }
